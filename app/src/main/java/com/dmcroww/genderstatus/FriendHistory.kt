04902e4d487ea21af943bf19cdd46c3ea8eeffc2
DMCroww <DMCroww@gmail.com>
2024-02-14 06:35:26 +0100

    Added FriendView pages and logic,adjusted history logic and functionality, renamed files to align with new app purpose

--- app/src/main/java/com/dmcroww/genderstatus/FriendHistory.kt
+++ app/src/main/java/com/dmcroww/genderstatus/FriendHistory.kt
@@ -28,16 +28,32 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
-private lateinit var appData: AppOptions
-private var finalColorIdx: Int = 0
-private lateinit var rotateAnimation: RotateAnimation
-
 class FriendHistory: AppCompatActivity() {
+	private val storageManager = StorageManager(applicationContext)
+	private val appData = AppOptions(applicationContext)
+	private val apiClient = ApiClient(applicationContext)
+
+	private lateinit var username: String
+	private val genders: Array<String> = resources.getStringArray(R.array.genders_array)
+	private val ages: Array<String> = resources.getStringArray(R.array.ages_array)
+
+	private val bigSize = 20.0f * (appData.fontSize / 100.0f)
+	private val medSize = 20.0f * (appData.fontSize / 100.0f)
+	private val smallSize = 18.0f * (appData.fontSize / 100.0f)
+	private val tinySize = 16.0f * (appData.fontSize / 100.0f)
+	private var finalColorIdx: Int = 0
+	private var rotateAnimation = RotateAnimation(
+		0f, 360f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f
+	).apply {
+		duration = 2000 // Set the duration of the rotation
+		repeatCount = -1 // Make the rotation continuous
+	}
 
 	private val dataUpdateReceiver = object: BroadcastReceiver() {
 		override fun onReceive(context: Context?, intent: Intent?) {
 			// Handle data update here, you can refresh the history
 			loading(true)
+
 			refreshHistory()
 		}
 	}
@@ -46,6 +62,7 @@ class FriendHistory: AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.friend_history)
+		username = intent.getStringExtra("username")!!
 
 		// Register the receiver for the broadcast
 		val filter = IntentFilter("com.dmcroww.genderstatus.DATA_UPDATED")
@@ -57,33 +74,27 @@ class FriendHistory: AppCompatActivity() {
 
 	@OptIn(DelicateCoroutinesApi::class)
 	private fun initializeUI() {
-		rotateAnimation = RotateAnimation(
-			0f, 360f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f
-		)
-		rotateAnimation.duration = 2000 // Set the duration of the rotation
-		rotateAnimation.repeatCount = -1 // Make the rotation continuous
-		loading()
-
-		appData = AppOptions.getData(applicationContext)
+		val backgroundArray = resources.obtainTypedArray(R.array.background_sources)
+		val colorsArray = resources.obtainTypedArray(R.array.color_sources)
 
 		// Determine default values based on system theme
 		val defaultBackgroundIdx = if (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES) 2 else 1
 
+		loading()
+
+		finalColorIdx = if (appData.textColorInt == 0 || appData.background == "") colorsArray.getColor(defaultBackgroundIdx, 0) else appData.textColorInt
+		findViewById<TextView>(R.id.title_history).setTextColor(finalColorIdx)
+
 		// Use the default values if the selected values are 0 (indicating "Default")
-		val backgroundArray = resources.obtainTypedArray(R.array.background_sources)
 		findViewById<ConstraintLayout>(R.id.main).setBackgroundResource(backgroundArray.getResourceId(defaultBackgroundIdx, 0))
 
 		GlobalScope.launch(Dispatchers.IO) {
-			val backgroundImage = if (appData.background != "") StorageManager.fetchImage(applicationContext, "backgrounds", appData.background) else null
+			val backgroundImage = if (appData.background != "") storageManager.fetchImage("backgrounds", appData.background) else null
 			runOnUiThread {
 				findViewById<ImageView>(R.id.backgroundImage).setImageBitmap(backgroundImage)
 			}
 		}
 
-		val colorsArray = resources.obtainTypedArray(R.array.color_sources)
-		finalColorIdx = if (appData.textColorInt == 0 || appData.background == "") colorsArray.getColor(defaultBackgroundIdx, 0) else appData.textColorInt
-
-		findViewById<TextView>(R.id.title_history).setTextColor(finalColorIdx)
 		backgroundArray.recycle()
 		colorsArray.recycle()
 	}
@@ -91,67 +102,52 @@ class FriendHistory: AppCompatActivity() {
 	@OptIn(DelicateCoroutinesApi::class)
 	@SuppressLint("UseCompatLoadingForDrawables", "SetTextI18n")
 	private fun refreshHistory() {
+		val historyDataLayout = findViewById<LinearLayout>(R.id.history_data)
+		historyDataLayout.removeAllViews()
+
 		GlobalScope.launch(Dispatchers.IO) {
-			val genders: Array<String> = resources.getStringArray(R.array.genders_array)
-			val ages: Array<String> = resources.getStringArray(R.array.ages_array)
-			val historyDataLayout = findViewById<LinearLayout>(R.id.history_data)
-			historyDataLayout.removeAllViews()
-
 			try {
-				val history = StorageManager.getPartnerHistory(applicationContext)
+				val history = apiClient.fetchFriendHistory(username)
 
-				val bigSize = 20.0f * (appData.fontSize / 100.0f)
-				val medSize = 20.0f * (appData.fontSize / 100.0f)
-				val smallSize = 18.0f * (appData.fontSize / 100.0f)
-				val tinySize = 16.0f * (appData.fontSize / 100.0f)
 
 				historyDataLayout.removeAllViews()
 
 				// Check if the stored string is not null
-				if (history.length() > 0) {
+				if (history.isEmpty()) {
+					Toast.makeText(applicationContext, "Friend history empty!", Toast.LENGTH_SHORT).show()
+					loaded()
+				} else {
 					// Inflate the template layout for each entry
-					for (i in 0 until history.length()) {
-						val historyObject = history.getJSONObject(i)
+					for (i in 0 until history.size) {
+						val status = history.get(i)
 
 						val entryView = LayoutInflater.from(applicationContext).inflate(R.layout.history_entry, historyDataLayout, false)
 
-						val timestamp = historyObject.optLong("timestamp", 0)
-						val avatar = historyObject.optString("avatar", "...")
-						val activity = historyObject.optString("activity", "...")
-						val mood = historyObject.optString("mood", "...")
-						val gender = historyObject.optInt("gender", 0)
-						val age = historyObject.optInt("age", 0)
-						val sus = historyObject.optInt("sus", 0)
-
 						// Populate the entryView with data from historyObject
-						val dataAvatar = entryView.findViewById<ImageView>(R.id.avatar)
-						val dataDate = entryView.findViewById<TextView>(R.id.date)
+
+						val image = storageManager.fetchImage("avatars", status.avatar)
+						if (image != null) entryView.findViewById<ImageView>(R.id.avatar).setImageBitmap(image)
+						else entryView.findViewById<ImageView>(R.id.avatar).setImageDrawable(getDrawable(R.drawable.android_128))
+
 						val dataActivity = entryView.findViewById<TextView>(R.id.activity)
-						val dataMood = entryView.findViewById<TextView>(R.id.mood)
-						val dataOther = entryView.findViewById<TextView>(R.id.other)
-
-						val image = StorageManager.fetchImage(applicationContext, "avatars", avatar.toString())
-						if (image != null) {
-							dataAvatar.setImageBitmap(image)
-						} else {
-							dataAvatar.setImageDrawable(getDrawable(R.drawable.android_128))
-						}
-
-						dataActivity.text = activity
+						dataActivity.text = status.activity
 						dataActivity.setTextColor(finalColorIdx)
 						dataActivity.textSize = bigSize
 
-						dataMood.text = mood
+						val dataMood = entryView.findViewById<TextView>(R.id.mood)
+						dataMood.text = status.mood
 						dataMood.setTextColor(finalColorIdx)
 						dataMood.textSize = medSize
 
-						dataOther.text = "${genders[gender]} ${ages[age]}, ${(sus - 1) * 10}% sus"
-						dataOther.setTextColor(finalColorIdx)
-						dataOther.textSize = smallSize
-
-						dataDate.text = timestampToRelativePlus(timestamp)
+						val dataDate = entryView.findViewById<TextView>(R.id.date)
+						dataDate.text = timestampToRelativePlus(status.timestamp)
 						dataDate.setTextColor(finalColorIdx)
 						dataDate.textSize = tinySize
+
+						val dataOther = entryView.findViewById<TextView>(R.id.other)
+						dataOther.text = "${genders[status.gender]} ${ages[status.age]}, ${(status.sus - 1) * 10}% sus"
+						dataOther.setTextColor(finalColorIdx)
+						dataOther.textSize = smallSize
 
 						runOnUiThread {
 							historyDataLayout.addView(entryView)
@@ -167,9 +163,6 @@ class FriendHistory: AppCompatActivity() {
 					runOnUiThread {
 						historyDataLayout.addView(space)
 					}
-					loaded()
-				} else {
-					Toast.makeText(applicationContext, "Partner history empty!", Toast.LENGTH_LONG).show()
 					loaded()
 				}
 			} catch (e: Exception) {
--- app/src/main/java/com/dmcroww/genderstatus/MainActivity.kt
+++ app/src/main/java/com/dmcroww/genderstatus/MainActivity.kt
@@ -14,30 +14,21 @@ import android.util.Log
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
 import androidx.lifecycle.lifecycleScope
+import androidx.viewpager.widget.ViewPager
 import kotlinx.coroutines.launch
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
 
 class MainActivity: AppCompatActivity() {
 	private var appData = AppOptions(applicationContext)
 	private var storageManager: StorageManager = StorageManager(applicationContext)
 	private var userData = UserData(applicationContext)
-	private val genders: Array<String> = resources.getStringArray(R.array.genders_array)
-	private val ages: Array<String> = resources.getStringArray(R.array.ages_array)
 
 	// Define UI elements
-	private lateinit var dataActivity: TextView
-	private lateinit var dataMood: TextView
-	private lateinit var dataAge: TextView
-	private lateinit var dataSus: TextView
-	private lateinit var dataGender: TextView
-	private lateinit var dataDelta: TextView
-	private lateinit var dataUpdated: TextView
 	private lateinit var buttonUpdate: ImageButton
 	private lateinit var buttonPreferences: ImageButton
-	private lateinit var buttonHistory: ImageButton
 	private val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
 	@SuppressLint("UnspecifiedRegisterReceiverFlag")
@@ -45,29 +36,21 @@ class MainActivity: AppCompatActivity() {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.main)
 		notificationManager.createNotificationChannel(NotificationChannel("friends_update_channel", "Friends Status Updates", NotificationManager.IMPORTANCE_LOW))
-
+		val viewPager: ViewPager = findViewById(R.id.Pager)
+		val adapter = SubScreenPagerAdapter(supportFragmentManager, userData.friends)
+		viewPager.adapter = adapter
 		// Initialize UI elements
-		dataActivity = findViewById(R.id.dataActivity)
-		dataMood = findViewById(R.id.dataMood)
-		dataAge = findViewById(R.id.dataAge)
-		dataSus = findViewById(R.id.dataSus)
-		dataGender = findViewById(R.id.dataGender)
-		dataDelta = findViewById(R.id.dataDelta)
-		dataUpdated = findViewById(R.id.dataUpdated)
 
 		buttonUpdate = findViewById(R.id.button_update)
 		buttonPreferences = findViewById(R.id.button_preferences)
-		buttonHistory = findViewById(R.id.button_history)
 
 		val serviceIntent = Intent(this, UpdateService::class.java)
 		val updateIntent = Intent(this, UpdateSelf::class.java)
 		val preferencesIntent = Intent(this, Preferences::class.java)
-		val historyIntent = Intent(this, FriendHistory::class.java)
 
 		startService(serviceIntent)
 		buttonUpdate.setOnClickListener {startActivity(updateIntent)}
 		buttonPreferences.setOnClickListener {startActivity(preferencesIntent)}
-		buttonHistory.setOnClickListener {startActivity(historyIntent)}
 
 		sendBroadcast(Intent("com.dmcroww.genderstatus.FORCE_UPDATE"))
 
@@ -100,8 +83,7 @@ class MainActivity: AppCompatActivity() {
 		}
 
 		registerReceiver(updateReceiver, filter)
-		// Read stored status, set theme, and set up intents
-		readStoredStatus()
+		setTheme()
 		Log.d("MAIN", "onResume()")
 	}
 
@@ -112,54 +94,30 @@ class MainActivity: AppCompatActivity() {
 		super.onStop()
 	}
 
+	private inner class SubScreenPagerAdapter(fm: FragmentManager, private val usernames: Array<String>):
+	 FragmentPagerAdapter(fm) {
+		override fun getItem(position: Int): Fragment {
+			// Create and return instance of SubScreenFragment with the username
+			return SubScreenFragment.newInstance(usernames[position])
+		}
+
+		override fun getCount(): Int {
+			// Return the number of sub-screens based on the number of usernames
+			return usernames.size
+		}
+	}
+
 	/**
 	 * Reads stored partner status from SharedPreferences and updates the UI elements.
 	 */
 	@SuppressLint("UseCompatLoadingForDrawables", "SetTextI18n")
 	private fun readStoredStatus() {
-		setTheme()
 
 		userData.friends.all {
-			val person = storageManager.loadPerson(it)
-			val personStatus = person.status
-
-			val delta = timestampToDelta(personStatus.timestamp)
-			val updated = timestampToDateTime(personStatus.timestamp)
-
-			lifecycleScope.launch {
-				val image = storageManager.fetchImage("avatars", personStatus.avatar)
-				if (image != null) {
-					findViewById<ImageView>(R.id.avatar).setImageBitmap(image)
-				} else {
-					findViewById<ImageView>(R.id.avatar).setImageDrawable(getDrawable(R.drawable.android_128))
-				}
-				dataActivity.text = personStatus.activity
-				dataMood.text = personStatus.mood
-				dataAge.text = ages[personStatus.age]
-				dataSus.text = if (personStatus.sus > 0) "${(personStatus.sus - 1) * 10}%" else "..."
-				dataGender.text = genders[personStatus.gender]
-				dataDelta.text = delta
-				dataUpdated.text = "($updated)"
-			}
 			true
 		}
 	}
 
-	/**
-	 * Converts a Unix timestamp to a formatted date and time string.
-	 */
-	private fun timestampToDateTime(unixTimestamp: Long): String {
-		return if (unixTimestamp > 0) SimpleDateFormat("HH:mm, dd.MM.yyyy", Locale.getDefault()).format(Date(unixTimestamp * 1000L))
-		else "..."
-	}
-
-	/**
-	 * Converts a Unix timestamp to a time difference to current time.
-	 */
-	private fun timestampToDelta(unixTimestamp: Long): String {
-		return if (unixTimestamp > 0) DateUtils.getRelativeTimeSpanString(unixTimestamp * 1000L, System.currentTimeMillis(), 60000L).toString()
-		else "..."
-	}
 
 	/**
 	 * Sets the app's theme based on user preferences.
@@ -184,50 +142,6 @@ class MainActivity: AppCompatActivity() {
 		backgroundArray.recycle()
 		colorsArray.recycle()
 
-		val titleActivity = findViewById<TextView>(R.id.title_activity)
-		val titleMood = findViewById<TextView>(R.id.title_mood)
-		val titleAge = findViewById<TextView>(R.id.title_age)
-		val titleSus = findViewById<TextView>(R.id.title_sus)
-		val titleGender = findViewById<TextView>(R.id.title_gender)
-		val titleUpdated = findViewById<TextView>(R.id.title_updated)
-
-		val bigSize = 32.0f * (appData.fontSize / 100.0f)
-		val medSize = 24.0f * (appData.fontSize / 100.0f)
-		val smallSize = 20.0f * (appData.fontSize / 100.0f)
-		val tinySize = 18.0f * (appData.fontSize / 100.0f)
-
-		titleActivity.setTextColor(finalColorIdx)
-		titleMood.setTextColor(finalColorIdx)
-		titleAge.setTextColor(finalColorIdx)
-		titleSus.setTextColor(finalColorIdx)
-		titleGender.setTextColor(finalColorIdx)
-		titleUpdated.setTextColor(finalColorIdx)
-		dataActivity.setTextColor(finalColorIdx)
-		dataMood.setTextColor(finalColorIdx)
-		dataAge.setTextColor(finalColorIdx)
-		dataSus.setTextColor(finalColorIdx)
-		dataGender.setTextColor(finalColorIdx)
-		dataDelta.setTextColor(finalColorIdx)
-		dataUpdated.setTextColor(finalColorIdx)
-
-		titleActivity.textSize = bigSize
-		titleMood.textSize = bigSize
-
-		dataActivity.textSize = medSize
-		dataMood.textSize = medSize
-
-		titleAge.textSize = smallSize
-		titleSus.textSize = smallSize
-		titleGender.textSize = smallSize
-		dataAge.textSize = smallSize
-		dataSus.textSize = smallSize
-		dataGender.textSize = smallSize
-
-		titleUpdated.textSize = smallSize
-		dataDelta.textSize = smallSize
-		dataUpdated.textSize = tinySize
-
-		buttonHistory.setColorFilter(finalColorIdx)
 		buttonUpdate.setColorFilter(finalColorIdx)
 		buttonPreferences.setColorFilter(finalColorIdx)
 	}
